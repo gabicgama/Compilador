b3b9f29b03b84e276ceb2a5c7a16453323f1ad25
gabicgama <gabycgama@hotmail.com>
2019-04-10 17:00:49 -0300

    new commit

---
+++ src/compilador/Tag.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+/**
+ *
+ * @author gabyc
+ */
+public class Tag {
+
+    public static final int 
+            //Palavras reservadas
+            APP = 256,
+            START = 257,
+            STOP = 258,
+            INTERGER = 259,
+            REAL = 260,
+            IF = 261,
+            THEN = 262,
+            END = 263,
+            ELSE = 264,
+            REPEAT = 265,
+            UNTIL = 266,
+            WHILE = 267,
+            DO = 268,
+            READ = 269,
+            WRITE = 270,
+            //Operadores e pontuações
+            ATB = 271,  //:=
+            GE = 272,   //>=
+            LE = 273,   //<=
+            NE = 274,   //!=
+            OR = 275,   //||
+            AND = 276,  //&&
+            //Identificadores e literais
+            IDF = 278,
+            INT_CONST = 279,
+            FLOAT_CONT = 280,
+            LIT = 281;
+            
+}
