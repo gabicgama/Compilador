424c043c9b1dcf7a5ec241a0d2dbd0d0a9448673
Kiqnetto <47460270+Kiqnetto@users.noreply.github.com>
2019-04-12 10:29:29 -0300

    Add files via upload
    Feito classes

---
+++ Num.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+/**
+ *
+ * @author kvnet
+ * @author gabycgama
+ */
+public class Num extends Token {
+
+    public final int value;
+
+    // Construtor
+    public Num(int v) {
+        super(Tag.INT_CONST);
+        value = v;
+    }
+    
+    public String toString(){
+        return "" + value; 
+    }
+    
+    
+}
