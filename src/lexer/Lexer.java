78ea3847cf364d5a8d5fc2dc9c47aaf18b6288ce
gabicgama <gabycgama@hotmail.com>
2019-04-14 17:35:46 -0300

    new commit

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -4,51 +4,33 @@
  * and open the template in the editor.
  */
 package lexer;
-
-import java.util.Hashtable;
+import java.io.*; 
+import java.util.*;
 
 /**
  *
- * @author gabyc
  * @author kvnet
+ * @author gabyc
  */
 public class Lexer {   
+    
     public static int line = 1;
-    //char peek = '';
+    char peek =' ';//peek = proximo
     Hashtable words = new Hashtable();
+    
     void reserve(Word w){
         words.put(w.lexeme, w);
     }
     
-    // Reserva palavras chaves selecionadas e objetos definidos em outras partes
     public Lexer(){
-        
-        // Palavras chaves selecionadas
-        reserve(new Word ("if", Tag.IF));
-        reserve(new Word ("else", Tag.ELSE));
-        reserve(new Word ("then", Tag.THEN));
-        reserve(new Word ("end", Tag.END));
-        reserve(new Word ("repeat", Tag.REPEAT));
-        reserve(new Word ("until", Tag.UNTIL));
-        reserve(new Word ("while", Tag.WHILE));
-        reserve(new Word ("do", Tag.DO));
-        reserve(new Word ("read", Tag.READ));
-        reserve(new Word ("write", Tag.WRITE));
-        reserve(new Word ("app", Tag.APP));
-        reserve(new Word ("start", Tag.START));
-        reserve(new Word ("stop", Tag.STOP));
-        reserve(new Word ("integer", Tag.INTEGER));
-        
-        // Objetos definidos em outras classes. Entra aqui: Types e objetos criados na classe Word
-        // TODO
-        
-        
+        //Palavras-chave
+        reserve(new Word("if", Tag.IF));    reserve(new Word("else", Tag.ELSE));
+        reserve(new Word("do", Tag.DO));    reserve(new Word("while", Tag.WHILE));
+        //Demais palavras reservadas
+        reserve(Word.app);      reserve(Word.start);    reserve(Word.stop);
+        reserve(Word.integer);  reserve(Word.real);     reserve(Word.then);
+        reserve(Word.end);      reserve(Word.repeat);   reserve(Word.until);
+        reserve(Word.read);     reserve(Word.write);   
     }
-    
-    /*
-    ** TODO
-    public Token scan(){
-       
-    }
-    */
+ 
 }
