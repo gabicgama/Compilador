7d2c629a80e98be91bfc3bf17f19556f3fa75c5a
gabicgama <gabycgama@hotmail.com>
2019-04-29 20:21:13 -0300

    new commit

--- src/compilador/Main.java
+++ src/compilador/Main.java
@@ -37,7 +37,7 @@ public class Main {
             System.out.println("Entrada inválida!");
         }
         
-        Lexer lexer= new Lexer(arq);
+        Lexer lexer = new Lexer(arq);
 
     }
 
